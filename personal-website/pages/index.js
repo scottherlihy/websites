d6b2d8761edf7300350cfedc8bd69a0ae40bc0fe
Scott <scottherlihy1@gmail.com>
2021-04-23 17:22:16 -0400

    Consistent grammar on home page

--- personal-website/pages/index.js
+++ personal-website/pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Hi I'm Scott, I’m interested in space, robotics, crypto, and urban infrastructure.</p>
-        <p>See what I'm building at my <a href="https://github.com/scottherlihy/scottherlihy"> github</a> and see the bad jokes I'm making on <a href="https://twitter.com/________scott"> twitter</a> </p>
+        <p>See what I'm building at my <a href="https://github.com/scottherlihy/scottherlihy"> github</a> and see the bad jokes I'm making on <a href="https://twitter.com/________scott"> twitter</a>.</p>
 
         <p>
           (This is a new site, much more to come soon)
