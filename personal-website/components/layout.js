f1d1c40aad84a4055e332191cae381db84c3e47e
Scott <scottherlihy1@gmail.com>
2021-04-24 22:14:52 -0400

    Update gerrit link, add sunburst component code
    - Sunburst code remains commented out because:
    https://github.com/fusioncharts/react-fusioncharts-component/issues/71

--- personal-website/components/layout.js
+++ personal-website/components/layout.js
@@ -14,7 +14,7 @@ export default function Layout({ children, home }) {
                 <link rel="icon" href="/favicon.ico" />
                 <meta
                     name="description"
-                    content="Learn how to build a personal website using Next.js"
+                    content="A personal website built using Next.js"
                 />
                 <meta
                     property="og:image"
--- personal-website/pages/index.js
+++ personal-website/pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
+import Portfolio from '../components/portfolio'
 import utilStyles from '../styles/utils.module.css'
 
 export default function Home() {
@@ -10,7 +11,7 @@ export default function Home() {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Hi I'm Scott, I’m interested in space, robotics, crypto, and urban infrastructure.</p>
-        <p>See what I'm building at my <a href="https://github.com/scottherlihy/scottherlihy"> github</a> and see the bad jokes I'm making on <a href="https://twitter.com/________scott"> twitter</a>.</p>
+        <p>See what I'm building at my <a href="https://github.com/scottherlihy"> github</a> and see the bad jokes I'm making on <a href="https://twitter.com/________scott"> twitter</a>.</p>
 
         <p>
           (This is a new site, much more to come soon)
