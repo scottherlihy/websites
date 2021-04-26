a076c449f6c27c259879237c45f7c8d35a537821
Scott <scottherlihy1@gmail.com>
2021-04-25 20:48:54 -0400

    Added links for future morning/portfolio pages

--- personal-website/pages/index.js
+++ personal-website/pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
-import Portfolio from '../components/portfolio'
+import Link from 'next/link'
 import utilStyles from '../styles/utils.module.css'
 
 export default function Home() {
@@ -17,6 +17,70 @@ export default function Home() {
           (This is a new site, much more to come soon)
         </p>
       </section>
+      {/* <Portfolio></Portfolio> */}
+
+      <div className="grid">
+        <Link href="/morning">
+          <a className="card">
+            <h3>Good Morning &rarr;</h3>
+            <p>An in depth dashboard I created for my morning routine.</p>
+          </a>
+        </Link>
+
+        <Link href="/portfolio">
+          <a className="card">
+            <h3>Portfolio &rarr;</h3>
+            <p>A vizualization I use to better understand my investment allocations.</p>
+          </a>
+        </Link>
+
+        <style jsx>{`
+        .grid {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          flex-wrap: wrap;
+
+          max-width: 800px;
+          margin-top: 3rem;
+        }
+
+        .card {
+          margin: 1rem;
+          flex-basis: 45%;
+          padding: 1.5rem;
+          text-align: left;
+          color: inherit;
+          text-decoration: none;
+          border: 1px solid #eaeaea;
+          border-radius: 10px;
+          transition: color 0.15s ease, border-color 0.15s ease;
+        }
+
+        .card:hover,
+        .card:focus,
+        .card:active {
+          color: #0070f3;
+          border-color: #0070f3;
+        }
+
+        .card h3 {
+          margin: 0 0 1rem 0;
+          font-size: 1.5rem;
+        }
+
+        .card p {
+          margin: 0;
+          font-size: 1.25rem;
+          line-height: 1.5;
+        }
+
+        .logo {
+          height: 1em;
+        }
+
+      `}</style>
+      </div>
     </Layout>
   )
 }
