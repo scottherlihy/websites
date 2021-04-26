46d573b891db0a21901c3ae882495665ef68161a
Scott <scottherlihy1@gmail.com>
2021-04-25 21:00:49 -0400

    Adjusting homepage margins

--- personal-website/pages/index.js
+++ personal-website/pages/index.js
@@ -12,10 +12,6 @@ export default function Home() {
       <section className={utilStyles.headingMd}>
         <p>Hi I'm Scott, I’m interested in space, robotics, crypto, and urban infrastructure.</p>
         <p>See what I'm building at my <a href="https://github.com/scottherlihy"> github</a> and see the bad jokes I'm making on <a href="https://twitter.com/________scott"> twitter</a>.</p>
-
-        <p>
-          (This is a new site, much more to come soon)
-        </p>
       </section>
       {/* <Portfolio></Portfolio> */}
 
@@ -42,7 +38,7 @@ export default function Home() {
           flex-wrap: wrap;
 
           max-width: 800px;
-          margin-top: 3rem;
+          margin-top: 2rem;
         }
 
         .card {
