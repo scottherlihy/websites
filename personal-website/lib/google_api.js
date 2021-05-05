610a3d9b91b2982f6d217a8c0fff1517e12c585f
Scott <scottherlihy1@gmail.com>
2021-05-04 20:07:36 -0400

    testing 5

--- personal-website/lib/google_api.js
+++ personal-website/lib/google_api.js
@@ -6,6 +6,7 @@ export async function getGSheetRows() {
     try {
         const scopes = ["https://www.googleapis.com/auth/spreadsheets.readonly"];
         const jwt = new google.auth.JWT(
+            // process.env.GOOGLE_SHEETS_CLIENT_EMAIL.replace(/\\n/g, ''),
             email,
             null,
             // we need to replace the escaped newline characters
@@ -16,7 +17,7 @@ export async function getGSheetRows() {
 
         const sheets = google.sheets({ version: "v4", auth: jwt });
         const response = await sheets.spreadsheets.values.get({
-            spreadsheetId: process.env.SPREADSHEET_ID.replace(/\\n/g, '\n'),
+            spreadsheetId: process.env.SPREADSHEET_ID.replace(/\\n/g, ''),
             range: "A1:K50",
         });
 
