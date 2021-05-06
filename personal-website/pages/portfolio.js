9ae4b1a01d20a9c0f066e4f240a564f389c29cde
Scott <scottherlihy1@gmail.com>
2021-05-06 14:09:34 -0400

    go back to incremental static generation

--- personal-website/pages/portfolio.js
+++ personal-website/pages/portfolio.js
@@ -1,29 +1,29 @@
 import PortfolioPieChart from '../components/portfolio'
 
 import { getGSheetRows } from "../lib/google_api";
-// export async function getStaticProps(context) {
-//     const rows = await getGSheetRows();
-
-//     return {
-//         props: {
-//             rows,
-//         },
-//         // Next.js will attempt to re-generate the page:
-//         // - When a request comes in
-//         // - At most once every second
-//         revalidate: 1, // In seconds
-//     };
-// }
-
-export async function getServerSideProps(context) {
+export async function getStaticProps(context) {
     const rows = await getGSheetRows();
 
     return {
         props: {
             rows,
-        }
+        },
+        // Next.js will attempt to re-generate the page:
+        // - When a request comes in
+        // - At most once every second
+        revalidate: 1, // In seconds
     };
 }
+
+// export async function getServerSideProps(context) {
+//     const rows = await getGSheetRows();
+
+//     return {
+//         props: {
+//             rows,
+//         }
+//     };
+// }
 
 export default function Portfolio({ rows }) {
     return (<PortfolioPieChart rows={rows}></PortfolioPieChart>);
